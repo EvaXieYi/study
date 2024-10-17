ddebc5ceabc1d311ed70398fff0c7c3ab68a2a48
EvaXieYi <3136383816@qq.com>
2024-10-17 22:39:32 +0800

    1

--- chapter_three/chapter_three_zeroDivide.py
+++ chapter_three/chapter_three_zeroDivide.py
@@ -1,6 +1,6 @@
 def abc(num,number1=10,**number3,):
     try:
-        return  number1 / number2
-    except ZeroDivisionError:
-           print("zerodivision")
+        return  number1 / num
+    except ZeroDivisionError as e:
+           print(e)
 abc(0,14)
