890020d9738d30e857c5a2846f6010946532566a
EvaXieYi <3136383816@qq.com>
2024-10-20 21:45:15 +0800

    使用列表，列表中删除赋值，列表复制和拼接

---
+++ chapter_four/hello_list.py
@@ -0,0 +1,17 @@
+squares = [x*2  for x in range(10) if x==0: x=""]
+print(squares)
+
+
+spam=['cat','bat','rat','elephant',[1,2,3,[4,5,6]]]
+print(spam[4][3][1])
+print(spam[1-1])
+print(spam[-1])
+print(spam[:])
+print(spam[0:5])
+print(len(spam))
+print('Hello',spam[0],sep=" ")
+def abc(*mc):
+    print(mc)
+
+
+abc(1,2,3,4,5)
